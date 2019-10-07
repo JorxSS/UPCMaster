bca235777cedc5eb4035c680c83077d55eb71f0a
JorxSS <49075678+JorxSS@users.noreply.github.com>
2019-10-07 21:04:57 +0200

    first commit TODO3

---
+++ C++ for Games/SDL Game 1 - Master/Module.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Application;
+
+class Module
+{
+public:
+
+	Module()
+	{
+	}
+
+	virtual bool Init() 
+	{
+		return true; 
+	}
+
+	// TODO 3: Create a new method "Start()" that should be calling all modules just before the first frame
+	virtual void Start() {
+		
+	}
+	virtual update_status Update()
+	{
+		return UPDATE_CONTINUE;
+	}
+
+	virtual bool CleanUp() 
+	{ 
+		return true; 
+	}
+};
